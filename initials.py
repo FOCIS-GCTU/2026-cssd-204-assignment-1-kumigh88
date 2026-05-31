df16562b6f37e0ee785d7a77584d5e48c9673031
kumigh88 <kumi.gh88@gmail.com>
2026-05-31 03:22:09 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Prints the initials SK in large stylized letters
+# Assignment Number: 2
+#
+# Name:SAMUEL KUMI
+# STUDENT ID: 2425401483
+# Email: 2425401483@live.gctu.edu.gh
+# Grader: Samuel
+#
+# On my honor, YOUR NAME, this programming
+# assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
 
+    print()
+    print("...SKS")
+    print()
+    print("...SSSSSSSSSSSS......KK....KK......SSSSSSSSSSSS.....")
+    print("...SSSSSSSSSSSS......KK...KK.......SSSSSSSSSSSS.....")
+    print("...SSSS..............KK..KK........SSSS.............")
+    print("...SSSS..............KK.KK.........SSSS.............")
+    print("......SSSSSS.........KKKK.............SSSSSS........")
+    print(".........SSSSSS......KKKK................SSSSSS.....")
+    print(".............SSSS....KK.KK...................SSSS...")
+    print(".............SSSS....KK..KK..................SSSS...")
+    print("...SSSSSSSSSSSS.....KK...KK.......SSSSSSSSSSSS...")
+    print("...SSSSSSSSSSSS.....KK....KK......SSSSSSSSSSSS...")
+    print()
 
-
-main()
+if __name__ == "__main__":
+    main()
